37c95e68752338875076a1038a9265c507760b06
IhorWenher <73756717+IhorWenher@users.noreply.github.com>
2021-05-18 23:59:21 +0300

    Добавил логику

--- index.js
+++ index.js
@@ -5,23 +5,59 @@
  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
  */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+//const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
 /*
  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
  * остатка % и делим его на количество миллисекунд в одном часе
  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
  */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
 /*
  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
  */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+//const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 
 /*
  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
  * миллисекунд в одной секунде (1000)
  */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+//const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+
+const timer = {
+    start() {
+        const startTime = Date.now();
+        
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = currentTime - startTime;
+            const timeComponents = getTimeComponents(deltaTime);
+            
+            console.log(timeComponents);
+            console.log(timeComponents.secs)
+        }, 1000);
+    }
+}
+
+
+function getTimeComponents(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    return { days, hours, mins, secs };
+}
+
+function updateClockface({ days, hours, mins, secs }) {
+    refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
+}
+
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+ 
+timer.start();
