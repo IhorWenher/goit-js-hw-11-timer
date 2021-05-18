9bee5585a906d8f7252445ea7a186097222380bf
IhorWenher <73756717+IhorWenher@users.noreply.github.com>
2021-05-19 01:48:57 +0300

    Робочая модель без настройки

--- index.js
+++ index.js
@@ -26,38 +26,79 @@
  */
 //const secs = Math.floor((time % (1000 * 60)) / 1000);
 
+const refs = {
+    startBtn: document.querySelector('[data-action-start]'),
+    stopBtn: document.querySelector('[data-action-stop]'),
+    dateInput: document.querySelector('.js-date-input'),
+    clockface: document.querySelector('.test-timer')
 
-const timer = {
+}
+
+class Timer {
+    constructor({ onTick }) {
+        this.intervalId = null;
+        this.isActive = false;
+        this.onTick = onTick;
+        this.init = onTick;
+    }
+
+    init() {
+        const time = this.getTimeComponents(0);
+        this.onTick(time);
+    }
+
     start() {
+        if (this.isActive) {
+            return;
+        }
+
         const startTime = Date.now();
-        
-        setInterval(() => {
+        this.isActive = true;
+
+        this.intervalId = setInterval(() => {
             const currentTime = Date.now();
+            this.isActive = true;
             const deltaTime = currentTime - startTime;
-            const timeComponents = getTimeComponents(deltaTime);
-            
-            console.log(timeComponents);
-            console.log(timeComponents.secs)
+            //const deltaMinus = startTime - currentTime;
+            const time = this.getTimeComponents(deltaTime);
+            //const timeComponentsMinus = getTimeComponents(deltaMinus);
+
+            this.onTick(time);
+            console.log(`${days}:${hours}:${mins}:${secs}`);
+
         }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+        const time = this.getTimeComponents(0);
+        this.onTick(time);
+    }
+
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        return { days, hours, mins, secs };
+    }
+
+
+    pad(value) {
+        return String(value).padStart(2, '0');
     }
 }
 
+const timer = new Timer({
+    onTick: updateClockface,
+});
 
-function getTimeComponents(time) {
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
 
-    return { days, hours, mins, secs };
-}
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
 
 function updateClockface({ days, hours, mins, secs }) {
     refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
 }
-
-function pad(value) {
-    return String(value).padStart(2, '0');
-}
- 
-timer.start();
